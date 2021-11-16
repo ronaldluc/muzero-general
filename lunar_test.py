3a6848250906e4c5a924ccc55b244312061d32ea
ronaldluc <ronald.luc@gmail.com>
2021-11-16 13:35:49 +0100

    feat(setup): add lunar test

---
+++ lunar_test.py
@@ -0,0 +1,12 @@
+import gym
+import torch
+
+env_to_wrap = gym.make("LunarLander-v2")
+env = gym.wrappers.Monitor(env_to_wrap, "special_dir", force=True)
+frame = env.reset()
+is_done = False
+while not is_done:
+  action = env.action_space.sample()
+  _, _, is_done, _ = env.step(action)
+env.close()
+env_to_wrap.close()
