47c0fb7ab9dc3f52325c98e8b9b4d76113b32f76
Demetrio Rodriguez T <Demetrio.Rodriguez.T@gmail.com>
2021-12-10 21:23:54 +0100

    drone that just flies -- EZ mode

---
+++ simulation/hover_drone.py
@@ -0,0 +1,20 @@
+"""
+"Drone" that has API compatible with our "Robot", except this one just flies wherever it's sent.
+Kinda like a drone that hovers.
+"""
+from simulation.robot_dynamics import Robot
+import numpy as np
+
+class Drone(Robot):
+    def step(self, dt):
+        self.hull.position[0] += self.wheels[2].gas/2
+        self.hull.position[1] += self.wheels[3].gas/2
+
+    def gas_wheel(self, gas, wheel_id):
+        wheel = self.wheels[wheel_id]
+        gas = np.clip(gas, -1, 1)
+        wheel.gas += gas
+
+    def brake(self, intensity):
+        for w in self.wheels:
+            w.gas = 0
